c12ed541dfa1e4925293876c427342c928bbb531
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-09-23 23:03:27 +0200

    Task 1 - done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+let changeColorInterval;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function hdrInterval() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+const btnStart = document.querySelector('[data-start]');
+btnStart.addEventListener('click', (e) => {
+  const element = e.currentTarget;
+  changeColorInterval = setInterval(hdrInterval, 1000);
+  element.setAttribute('disabled', '');
+});
+
+const btnStop = document.querySelector('[data-stop]');
+btnStop.addEventListener('click', () => {
+  clearInterval(changeColorInterval);
+  btnStart.removeAttribute('disabled');
+});
